5d8a224d3b529cf2a882f950588318008f3cacaf
SongYeChan1024 <swq3913@gmail.com>
2026-01-26 11:46:18 +0900

    적 추적 사항 사이드 스크롤에 맞게 수정
    이후 공격 애니메이션과 피격 애니메이션 넣을것들
    준비해놓음

--- Assets/02_Scripts/Enemy Ctrl.cs
+++ Assets/02_Scripts/Enemy Ctrl.cs
@@ -8,34 +8,46 @@ public class EnemyCtrl : MonoBehaviour
 {
     public NavMeshAgent navi; //내비
     //public Animator animator; // 애니메이터
+    [Header("적 추적 범위와 공격 범위")]
     public float traceDist = 50f; //인식 범위,추적 범위
     public float attackDist = 2f;
+    [Header("Z축 고정")]
+    public float lockZ = 0f; //적 Z축 고정 위치
+    public bool lockZAxis = true; //Z축 고정 사용 여부 
     public Transform enemyTr; // 에너미 위치
     public Transform playerTr; // 플레이어 위치
+    private bool IsAttacking = false;
     void Start()
     {
         navi = this.gameObject.GetComponent<NavMeshAgent>();
         //animator = GetComponent<Animator>();
         enemyTr = GetComponent<Transform>();
         playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
-    }                 //플레이어 태그로 플레이어와 에너미의 거리 인식
+                              //플레이어 태그로 플레이어와 에너미의 거리 인식
+        navi.updateRotation = false;
+        navi.updateUpAxis = false;
+    }                 
 
     void Update()
     {
-        if (!navi.enabled || !navi.isOnNavMesh || playerTr == null) return;
-        float distance = Vector3.Distance(enemyTr.position, playerTr.position);
-        
+        if (playerTr == null) return;
+        if (!navi.enabled || !navi.isOnNavMesh) return;
+        //플레이어 목적지 Z축 고정
+           Vector3 playerPos = playerTr.position;
+        if (lockZAxis) playerPos.z = lockZ;
+        //거리계산도 고정된 자표 기준으로 계싼
+        float distance = Vector3.Distance(enemyTr.position, playerPos);
+        //공격 거리면 정지
         if(distance <= attackDist)
         {
-            navi.isStopped = true;
-            navi.ResetPath();
+            StopAgent();
             //animator.SetBool("IsTrace",false);
             //animator.SetBool("IsAttack",true);
         }
-        if (distance <= traceDist)
+        else if (distance <= traceDist)
         {
             navi.isStopped = false;
-            navi.SetDestination(playerTr.position);
+            navi.SetDestination(playerPos);
             //animator.SetBool("IsAttack", false);
             //animator.SetBool("IsTrace", true);
         }
@@ -46,5 +58,26 @@ public class EnemyCtrl : MonoBehaviour
             //animator.SetBool("IsAttack", false);
             //animator.SetBool("IsTrace", false);
         }
+        //에너미 Z축 고정
+        if(lockZAxis) FixZ();
+        FaceToPlayer();
+    }
+    void StopAgent()
+    {
+        navi.isStopped = true;
+        navi.ResetPath();
+    }
+    void FixZ()
+    {
+        Vector3 pos = transform.position;
+        pos.z = lockZ;
+        transform.position = pos;
+    }
+    void FaceToPlayer()
+    {
+        if(playerTr.position.x > transform.position.x)
+        transform.rotation = Quaternion.Euler(0, 90f, 0);
+        else
+        transform.rotation = Quaternion.Euler(0, -90f, 0);
     }
 }
