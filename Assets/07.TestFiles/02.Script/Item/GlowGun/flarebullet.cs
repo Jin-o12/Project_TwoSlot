e22f5c0dbf497de8c7e11e8589c6129e75cab323
SongYeChan1024 <swq3913@gmail.com>
2026-01-28 17:07:51 +0900

    아이템 세가지 구현

--- Assets/07.TestFiles/02.Script/Item/GlowGun/flarebullet.cs
+++ Assets/07.TestFiles/02.Script/Item/GlowGun/flarebullet.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
 public class FlareBullet : MonoBehaviour
 {
     [Header("Lifetime")]
-    public float flareTimer = 9f;          // 타는 시간
-    public float destroyDelay = 1f;        // 꺼진 뒤 삭제 여유
+    public float flareTimer = 9f;      // 타는 시간
+    public float destroyDelay = 1f;    // 꺼진 뒤 삭제 여유
 
-    [Header("Flicker While Burning")]
+    [Header("Flicker")]
     public Vector2 burnIntensityRange = new Vector2(2f, 6f);
+    public Vector2 flyIntensityRange  = new Vector2(1.2f, 2.2f); // 날아갈 때는 약하게
+    public bool flickerStrongerAfterHit = true;
 
     [Header("Fade Out")]
     public float smooth = 2.4f;
@@ -24,10 +25,11 @@ public class FlareBullet : MonoBehaviour
     ParticleSystemRenderer _psr;
 
     bool _burning = true;
+    bool _hasHitSomething = false;
 
     void Awake()
     {
-        _flareLight = GetComponentInChildren<Light>(); // 혹시 자식에 있을 수도 있어서
+        _flareLight = GetComponentInChildren<Light>();
         _audio = GetComponent<AudioSource>();
         _ps = GetComponentInChildren<ParticleSystem>();
         if (_ps != null) _psr = _ps.GetComponent<ParticleSystemRenderer>();
@@ -38,8 +40,18 @@ public class FlareBullet : MonoBehaviour
         if (flareBurningSound != null)
             _audio.PlayOneShot(flareBurningSound);
 
-        StartCoroutine(BurnThenFade());
+        // flareTimer 뒤에 "꺼짐" 시작
+        Invoke(nameof(BeginFadeOut), flareTimer);
+
+        // 전체 삭제 시간
         Destroy(gameObject, flareTimer + destroyDelay);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // 첫 충돌 체크 (바닥/벽 닿았는지)
+        if (!_hasHitSomething)
+            _hasHitSomething = true;
     }
 
     void Update()
@@ -48,8 +60,16 @@ public class FlareBullet : MonoBehaviour
 
         if (_burning)
         {
-            // 타는 동안 살짝 깜빡
-            _flareLight.intensity = Random.Range(burnIntensityRange.x, burnIntensityRange.y);
+            // 타는 동안 깜빡임
+            Vector2 range = burnIntensityRange;
+
+            // 충돌 전에는 덜 깜빡이게
+            if (!_hasHitSomething)
+                range = flyIntensityRange;
+            else if (!flickerStrongerAfterHit)
+                range = flyIntensityRange;
+
+            _flareLight.intensity = Random.Range(range.x, range.y);
         }
         else
         {
@@ -63,13 +83,23 @@ public class FlareBullet : MonoBehaviour
             // 파티클 크기 줄이기 (있는 경우만)
             if (_psr != null)
                 _psr.maxParticleSize = Mathf.Lerp(_psr.maxParticleSize, 0f, Time.deltaTime * particleFadeSpeed);
+
+            // (선택) 파티클 방출도 줄이고 싶으면 아래를 추가할 수 있어:
+            // if (_ps != null)
+            // {
+            //     var em = _ps.emission;
+            //     em.rateOverTime = Mathf.Lerp(em.rateOverTime.constant, 0f, Time.deltaTime * particleFadeSpeed);
+            // }
         }
     }
 
-    IEnumerator BurnThenFade()
+    void BeginFadeOut()
     {
-        _burning = true;
-        yield return new WaitForSeconds(flareTimer);
         _burning = false;
+    }
+
+    void OnDestroy()
+    {
+        CancelInvoke();
     }
 }
