bc4f29c8b5eb78698851388a83a97b580994fb08
SongYeChan1024 <swq3913@gmail.com>
2026-01-28 14:49:36 +0900

    아이템 사용 구현 전반에대한것과 정리 한다고 했는데 오히려 방해 될지도..

---
+++ Assets/07.TestFiles/02.Script/Item/GlowGun/flarebullet.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class FlareBullet : MonoBehaviour
+{
+    [Header("Lifetime")]
+    public float flareTimer = 9f;          // 타는 시간
+    public float destroyDelay = 1f;        // 꺼진 뒤 삭제 여유
+
+    [Header("Flicker While Burning")]
+    public Vector2 burnIntensityRange = new Vector2(2f, 6f);
+
+    [Header("Fade Out")]
+    public float smooth = 2.4f;
+    public float particleFadeSpeed = 5f;
+
+    [Header("Audio")]
+    public AudioClip flareBurningSound;
+
+    Light _flareLight;
+    AudioSource _audio;
+    ParticleSystem _ps;
+    ParticleSystemRenderer _psr;
+
+    bool _burning = true;
+
+    void Awake()
+    {
+        _flareLight = GetComponentInChildren<Light>(); // 혹시 자식에 있을 수도 있어서
+        _audio = GetComponent<AudioSource>();
+        _ps = GetComponentInChildren<ParticleSystem>();
+        if (_ps != null) _psr = _ps.GetComponent<ParticleSystemRenderer>();
+    }
+
+    void Start()
+    {
+        if (flareBurningSound != null)
+            _audio.PlayOneShot(flareBurningSound);
+
+        StartCoroutine(BurnThenFade());
+        Destroy(gameObject, flareTimer + destroyDelay);
+    }
+
+    void Update()
+    {
+        if (_flareLight == null) return;
+
+        if (_burning)
+        {
+            // 타는 동안 살짝 깜빡
+            _flareLight.intensity = Random.Range(burnIntensityRange.x, burnIntensityRange.y);
+        }
+        else
+        {
+            // 꺼지면서 감쇠
+            _flareLight.intensity = Mathf.Lerp(_flareLight.intensity, 0f, Time.deltaTime * smooth);
+            _flareLight.range     = Mathf.Lerp(_flareLight.range,     0f, Time.deltaTime * smooth);
+
+            if (_audio != null)
+                _audio.volume = Mathf.Lerp(_audio.volume, 0f, Time.deltaTime * smooth);
+
+            // 파티클 크기 줄이기 (있는 경우만)
+            if (_psr != null)
+                _psr.maxParticleSize = Mathf.Lerp(_psr.maxParticleSize, 0f, Time.deltaTime * particleFadeSpeed);
+        }
+    }
+
+    IEnumerator BurnThenFade()
+    {
+        _burning = true;
+        yield return new WaitForSeconds(flareTimer);
+        _burning = false;
+    }
+}
---
+++ Assets/07.TestFiles/02.Script/Player/PlayerItem/flareround.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerItemUse : MonoBehaviour
+{
+    public PlayerItemTrigger inv;
+
+    [Header("Use Key")]
+    public KeyCode useKey = KeyCode.Mouse0; // 좌클릭 (원하면 F/우클릭으로 바꿔)
+
+    [Header("FlareGun 1회용")]
+    public string flareGunItemId = "FlareGun";
+    public OneShotFlareGun heldFlareGun;    // 손에 들고 있는 플레어건(모델) 오브젝트에 붙어있는 스크립트
+
+    void Awake()
+    {
+        if (inv == null) inv = GetComponent<PlayerItemTrigger>();
+    }
+
+    void Update()
+    {
+        if (!Input.GetKeyDown(useKey)) return;
+        if (inv == null) return;
+
+        string selected = inv.GetSelectedItem();
+        if (selected != flareGunItemId) return;
+
+        // 1) 발사
+        if (heldFlareGun != null)
+            heldFlareGun.FireOnce();
+
+        // 2) 아이템 소비(슬롯에서 제거)
+        inv.TryConsume(flareGunItemId);
+
+        // 3) 한발쏘고나면 플레어건 자체가 사라짐
+		
+        if (heldFlareGun != null)
+            heldFlareGun.gameObject.SetActive(false);
+    }
+}
